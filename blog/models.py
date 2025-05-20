70e120ea5fd8c5adbb214345bf74faa37407a66f
oleh.koval <oleh.koval@nszu.gov.ua>
2025-05-20 10:26:44 +0300

    created model Post

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    body = models.TextField()
+
+    # дата та время публікації поста
+    publish = models.DataTimeField(default=timezone.now)
+    # дата та время створення поста
+    created = models.DataTimeField(auto_now_add=True)
+    # дата та время останнього оновлення поста
+    updated = models.DataTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
